2fbc9b464a26eed80f79295d9d448193ada70390
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2020-07-20 22:16:50 -0400

    Subject counter form and categories

--- usestate-counter/src/App.js
+++ usestate-counter/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Example from './components/Example'
+import Form from './components/Form'
 
 function App() {
   return (
     <div className="App">
-      <Example/>
+      <Form/>
     </div>
   );
 }
--- usestate-counter/src/components/Category.js
+++ usestate-counter/src/components/Category.js
@@ -1,7 +1,19 @@
     const topics = [
-        'Coding', 'Calligraphy', 'Cycling' 
+        'Coding', 'Calligraphy', 'Physical Wellness', 'Mental Wellness' 
     ]
 
     const coding = [
         'interview', 'technical' ,'cultural', 'JavaScript', "React"
+    ]
+
+    const calligraphy = [
+        'miniscule', 'majuscule', 'copperplate', 'spencerian', 'drills', 'flourish', 'instagram'
+    ]
+
+    const physical = [
+        'cyling', 'swimming', 'tennis', 'rock climbing', 'scuba diving', 'yoga'
+    ]
+
+    const mental = [
+        'headspace', 'reflectly', 'photo journaling'
     ]

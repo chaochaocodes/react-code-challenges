d0cbc39fac2200d3649f4af8e8486f203b1bd32f
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2020-07-20 22:08:16 -0400

    Subject Main card UI Problem refactored out into separate component and css file

---
+++ useeffect-api/src/components/CardUI.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import './cardui.css'
+
+const CardUI = ({data, getIngredients, callAPI}) => {
+
+    const ingredientList = getIngredients(data).map((item, index) =>
+        <li key={index.toString()}> {item.charAt(0).toUpperCase() + item.slice(1)}</li>);
+    
+    return (
+        <>
+        <h1> Drink of the Day</h1>
+        <div className="card-ui">
+            <div className="media">
+                <img src={data.strDrinkThumb} alt={DataTransfer.strDrink}/>
+            </div>
+            <div className="card-body">
+                <h2> {data.strDrink} </h2>
+                <div className="divider"/>
+                <p> Ingredients </p>
+                {ingredientList}
+                <button className="card-btn" onClick={ () => callAPI() }> 
+                {'>'}
+                </button>
+                {/* <button className="card-btn"> Show Recipe </button> */}
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default CardUI;
+
+
+

2c49fcb0ea9f036e4e0ad77c1e97e87370777245
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2020-07-22 04:36:54 -0400

    Subject menu bar

---
+++ useeffect-api/src/components/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './menu.css'
+
+const Menu = () => {
+
+    return(
+        <div className="menu-btn">
+            <button> INDEX </button>
+            <button> CATEGORY </button>
+            <button> SEARCH </button>
+        </div>
+    )
+}
+
+export default Menu;

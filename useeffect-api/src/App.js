4cc4447789c1448710ca57b2fe7733cf23dbc2a6
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2020-07-23 15:20:43 -0400

    Subject Added toggle to menu buttons Problem index finally displays! but only after dictinary loads

--- useeffect-api/src/App.js
+++ useeffect-api/src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState }from 'react';
 import Cocktail from './components/Cocktail';
+import Menu from './components/Menu';
+import DrinkIndex from './components/DrinkIndex';
+import Learn from './components/Learn';
+import Jokes from './components/Jokes';
 
 function App() {
+
+  const [toggleIndex, setToggleIndex] = useState(false); 
+  const [toggleLearn, setToggleLearn] = useState(false);
+
+  const toggleIndexButton = () => {
+    setToggleIndex(!toggleIndex);
+  }
+  const toggleLearnButton = () => {
+    setToggleLearn(!toggleLearn);
+}
   return (
     <div className="App">
       <Cocktail/>
+      <Menu
+        toggleIndexButton={toggleIndexButton}
+        toggleLearnButton={toggleLearnButton} />
+      {toggleIndex && <DrinkIndex />}
+      {toggleLearn && <Learn/>}
+      {/* <Jokes/> */}
     </div>
   );
 }

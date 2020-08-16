94aa269ab0b155faf9fca0748dcd07c48fac5afe
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2020-08-16 00:17:16 -0400

    Subject basic form Problem show info after submitting input Solution useState hook

--- useref-form/src/App.js
+++ useref-form/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [button, setButton] = useState(null);
+
+  const onClick = (e) => {
+    e.preventDefault();
+    setButton(true)
+  }
+
+  const showInfo = () => {
+    return (
+    <ul>
+      <li>Name: {name}</li>
+      <li>Phone: {phone}</li>
+      <li>Email: {email}</li>
+    </ul>
+    )
+  }
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Contact List</h1>
+      <h3> Enter your Information</h3>
+      <input 
+        type="text" 
+        value={name} 
+        onChange={e => setName(e.target.value)}
+        name="name" 
+        placeholder="name"/><br/>
+      <input 
+        type="text" 
+        value={phone} 
+        onChange={e => setPhone(e.target.value)}
+        name="phone" 
+        placeholder="phone"/><br/>
+      <input 
+        type="text" 
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        name="email"
+        placeholder="email"/><br/>
+      <button type="submit" onClick={onClick}>
+          Submit
+      </button>
     </div>
-  );
+    <div>
+      {button ? showInfo() : null }
+    </div>
+    </>
+  )
 }
 
 export default App;
